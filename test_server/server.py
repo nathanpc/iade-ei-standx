3be9c903f8396b06ffc48b8fdafc0d7339543a57
Nathan Campos <nathan@innoveworkshop.com>
2025-10-15 18:31:26 +0100

    Created a simple test server for testing

---
+++ test_server/server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/cars')
+def list_cars():
+    return {
+        'cars': [
+            {
+                'id': 567,
+                'make': 'Kia',
+                'model': 'Sorento',
+                'year': '2015',
+                'km': 56789,
+                'price': 12100,
+                'transmission': 'Manual',
+                'fuel': 'GPL',
+                'seats': 7
+            },
+            {
+                'id': 123,
+                'make': 'Mini',
+                'model': 'Cooper',
+                'year': '2008',
+                'km': 123456,
+                'price': 8000,
+                'transmission': 'Auto',
+                'fuel': 'Diesel',
+                'seats': 5
+            }
+        ],
+        'test': 'whatever'
+    }
