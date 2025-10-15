74cea53f6a742f8921907bd718b2f4617df61439
Nathan Campos <nathan@innoveworkshop.com>
2025-10-15 18:32:34 +0100

    Imported Fuel library for network communication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.fuel)
+    implementation(libs.fuel.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
