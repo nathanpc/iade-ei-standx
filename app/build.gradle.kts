88187fa99e13f1b1e064a52bd6fae57efcd7e0b4
Nathan Campos <nathan@innoveworkshop.com>
2025-10-22 17:28:17 +0100

    Added libraries for JSON parsing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation(libs.gson)
     implementation(libs.fuel)
     implementation(libs.fuel.android)
+    implementation(libs.fuel.gson)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
